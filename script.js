8afc7ec6bd3d69658326550ee38b6a62a3555258
JeanSouza911 <shadowjean911@gmail.com>
2021-03-02 20:45:01 -0300

    alterar cores e adicionar README

--- script.js
+++ script.js
@@ -13,19 +13,19 @@ let food ={
 }
 
 function criarBG() {
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "white";
     context.fillRect(0, 0, 16 * box, 16 *box);
 }
 
 function criarCobrinha(){
     for(i=0; i < snake.length; i++){
-        context.fillStyle = "green";
+        context.fillStyle = "black";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
 function drawFood(){
-    context.fillStyle = "red";
+    context.fillStyle = "gray";
     context.fillRect(food.x, food.y, box, box);
 }
 
